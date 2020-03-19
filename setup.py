99b2627b71ba96261ed71657b81705833dfe22c6
Jeremy Hanmer <jeremy.hanmer@dreamhost.com>
2020-03-19 13:43:49 -0700

    pin eventlet

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         'docopt==0.6.1',
         'dpkt==1.8.6.2',
-        'eventlet>=0.17',
+        'eventlet<=0.18',
         'protobuf==2.4.1',
         'python-daemon==1.5.5'
     ],
